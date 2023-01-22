e0a13cbc6de8aa856758bc17594dff7e817f2fce
MAYCHINE MOHAMED <31590745+MedMaychin@users.noreply.github.com>
2023-01-22 17:10:35 +0100

    skills
    skills

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
-
+import Skills from "./Components/Skills/Skills";
 
 const App = () => {
   return (
     <>
       <Header />
-    
+
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   );
